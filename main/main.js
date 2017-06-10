d12862c959d2381a9cfd2712d62feec452f32da3
Christine233 <1040551136@qq.com>
2017-06-11 01:45:50 +0800

    main

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
+module.exports = function main(a) {
   // Write your cade here
+	var m = a.toString();
+	var n = 0;
+	for(n; m>0; m=parseInt(m/10)){
+		n += m%10;
+	}
+	return n;
 };
